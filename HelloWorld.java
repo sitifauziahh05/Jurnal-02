bc1d705124e3402bddf75db996317b96a4bd6837
sitifauziahh05 <sitifauziah@student.telkomuniversity.ac.id>
2021-10-04 22:31:00 +0700

    HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+public static void main (String[] args) {
+System.out.println("Nama : Siti Fauziah");
+System.out.println("NIM : 6706210130");
+System.out.println("Kelas : D3 RPLA 45-05");
+System.out.println("IPK : 4.00");
+}
+}
